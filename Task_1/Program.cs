fa6f727c08d23bc21f5eab1de416cade1fe94bc9
Oleg <ordo1882@gmail.com>
2023-07-09 14:49:28 +0200

    Added Task 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Enter a three-digit number:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (((n > 99) && (n < 1000) || ((n < -99) && (n > -1000)))) // Учитываем также негативное число
+{
+    n = n / 10 * -1; // В случае негативного числа умножаем на -1
+    n = n % 10;
+    Console.WriteLine($"The second digit is {n}");
+}
+else
+{
+    Console.WriteLine($"{n} is not a three-digit number.");
+}
